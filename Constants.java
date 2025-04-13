d485f66cbffbbfb93bdfd70d788789e7c75cf29e
Lilja <lilja.maria.kiiski@gmail.com>
2025-04-12 21:01:35 -0700

    Got images to load

--- Constants.java
+++ Constants.java
@@ -1,8 +1,8 @@
 import java.awt.Rectangle;
 
 public class Constants {
-	public static final int FRAME_WIDTH = 1920;
-	public static final int FRAME_HEIGHT = 1080;
+	public static final int FRAME_WIDTH = 1000;
+	public static final int FRAME_HEIGHT = 800;
 	public static final Rectangle FRAME_BOUNDS = new Rectangle(0, 0, FRAME_WIDTH, FRAME_HEIGHT);
 	public static final int DELAY = 100;
 	public static final boolean PRINTING = true;
